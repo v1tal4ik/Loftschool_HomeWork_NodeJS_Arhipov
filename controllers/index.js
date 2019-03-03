a073e29f69888df201035773c2cfa38f5cc8256a
v1tal4ik <vetalgta@gmail.com>
2019-03-03 11:23:21 +0200

    v.4.0

--- controllers/index.js
+++ controllers/index.js
@@ -8,22 +8,21 @@ const validate = require('../libs/validate');
 const nodemailer = require('nodemailer');
 
 
-
-module.exports.index = async(req,res, next)=>{
+//flash
+module.exports.index = async(ctx, next)=>{
     const products = db.getState().product || [];
     const skills = db.getState().skills || [];
     const data = {
         items_products:products,
         items_skills:skills,
-        msgsemail:req.flash('info')[0]
+        msgsemail:'message'
     }
-    res.render('pages/index.pug',data);
+    ctx.render('pages/index.pug',data);
 };
 
-
-
-module.exports.indexMessage = async(req,res, next)=>{
-    const {name,email,message} = req.body;
+// flash , redirect(#status)
+module.exports.indexMessage = async(ctx, next)=>{
+    const {name,email,message} = ctx.request.body;
     const transporter = nodemailer.createTransport(config.mail.smtp);
     const mailOptions = {
         from: `"${name}"`,
@@ -34,39 +33,109 @@ module.exports.indexMessage = async(req,res, next)=>{
     if(name&& email && message){
         transporter.sendMail(mailOptions,(error,info)=>{
             if(error){
-                req.flash('info','На жаль виникла помилка :(');
-                return res.redirect('/#status');
+                //req.flash('info','На жаль виникла помилка :(');
+                return ctx.redirect('/');
             }
 
 
-            req.flash('info','Повідомлення успішно відправлено');
-                return res.redirect('/#status');
+            //req.flash('info','Повідомлення успішно відправлено');
+                console.log('Повідомлення успішно відправлено');
+                return ctx.redirect('/');
         });
     }else{
-        req.flash('info','Заповніть усі поля ');
-                return res.redirect('/#status');
+        //req.flash('info','Заповніть усі поля ');
+                return ctx.redirect('/');
     }
 };
 
-
-module.exports.login = async(req,res, next)=>{
+//flash,session
+module.exports.login = async(ctx, next)=>{
     const data = {
-        msgslogin: req.flash('auth')[0]
+        msgslogin: ""
     }
-    if(req.session.auth){res.redirect('/admin')};
-    res.render('pages/login',data);
+    if(ctx.session.auth){ctx.redirect('/admin')};
+    ctx.render('pages/login',data);
 };
-
-module.exports.admin = async(req,res, next)=>{
+//flash
+module.exports.admin = async(ctx, next)=>{
     const data = {
-        msgskill: req.flash('skill')[0],
-        msgfile: req.flash('file')[0]
+        msgskill: '',
+        msgfile: ''
     }
-    res.render('pages/admin',data);
+    ctx.render('pages/admin',data);
+};
+//flash,session
+module.exports.auth = async(ctx, next)=>{
+    console.log('work');
+   const {email,password} = ctx.request.body;
+    const user = db.getState().user;
+    if(!email && !password){
+        //req.flash('auth','Заповніть усі поля');
+        res.redirect('/login');
+    }
+    if(user.email === email && psw.validPassword(password)){
+        console.log('before :',ctx.session.auth);
+        ctx.session.auth= true;
+        console.log('after :',ctx.session.auth);
+        ctx.redirect('/admin');
+    }else{
+        //req.flash('auth','Невірний логін або пароль');
+        ctx.redirect('/login');
+    }
+};
+//flash
+module.exports.adminSkills = async(ctx, next)=>{
+    const {age,concerts,cities,years} = ctx.request.body;
+    const skills = db.getState().skills;
+    if(age && concerts && cities && years){
+        db.set('skills[0].number',age).write();
+        db.set('skills[1].number',concerts).write();
+        db.set('skills[2].number',cities).write();
+        db.set('skills[3].number',years).write();
+        //req.flash('skill','Зміни збережено');
+        console.log('Зміни збережено')
+        ctx.redirect('/admin');
+    }else{
+        //req.flash('skill','Заповніть усі поля');
+        console.log("Заопвніть усі поля");
+        ctx.redirect('/admin');
+    }
+};
+//bodyParser,flash,
+module.exports.adminUpload = async(ctx, next)=>{
+    const {name,price} = ctx.request.body;
+    console.log(ctx.request);
+    /*const form = new formidable.IncomingForm();
+
+    form.uploadDir = path.join(process.cwd(),config.upload);
+
+    form.parse(req,(err,fields,files)=>{
+        if(err){console.log(err)};
+
+        const {name,price} = fields;
+        const result = validate(fields,files);
+        if(!result){
+            //req.flash('file','Виникла помилка :(');
+            ctx.redirect('/admin');
+        }else{
+            old_path = files.photo.path;
+            new_path = path.join(process.cwd(),config.upload,files.photo.name);
+            fs.renameSync(old_path,new_path);
+            let obj = {
+                src: `./upload/${files.photo.name}`,
+                price: price,
+                name: name
+            }
+            db.get('product').push(obj).write();
+            //req.flash('file','Товар успішно завантажено');
+            ctx.redirect('/admin');
+        }
+    });*/
 };
 
-module.exports.auth = async(req,res, next)=>{
-    const {email,password} = req.body;
+
+/*
+ const {email,password} = req.body;
     const user = db.getState().user;
     if(!email && !password){
         req.flash('auth','Заповніть усі поля');
@@ -79,49 +148,5 @@ module.exports.auth = async(req,res, next)=>{
         req.flash('auth','Невірний логін або пароль');
         res.redirect('/login');
     }
-};
 
-module.exports.adminSkills = async(req,res, next)=>{
-    const {age,concerts,cities,years} = req.body;
-    const skills = db.getState().skills;
-    if(age && concerts && cities && years){
-        db.set('skills[0].number',age).write();
-        db.set('skills[1].number',concerts).write();
-        db.set('skills[2].number',cities).write();
-        db.set('skills[3].number',years).write();
-        req.flash('skill','Зміни збережено');
-        res.redirect('/admin');
-    }else{
-        req.flash('skill','Заповніть усі поля');
-        res.redirect('/admin');
-    }
-};
-
-module.exports.adminUpload = async(req,res, next)=>{
-    const form = new formidable.IncomingForm();
-
-    form.uploadDir = path.join(process.cwd(),config.upload);
-
-    form.parse(req,(err,fields,files)=>{
-        if(err){console.log(err)};
-
-        const {name,price} = fields;
-        const result = validate(fields,files);
-        if(!result){
-            req.flash('file','Виникла помилка :(');
-            res.redirect('/admin');
-        }else{
-            old_path = files.photo.path;
-            new_path = path.join(process.cwd(),config.upload,files.photo.name);
-            fs.renameSync(old_path,new_path);
-            let obj = {
-                src: `./upload/${files.photo.name}`,
-                price: price,
-                name: name
-            }
-            db.get('product').push(obj).write();
-            req.flash('file','Товар успішно завантажено');
-            res.redirect('/admin');
-        }
-    });
-};
+    */
